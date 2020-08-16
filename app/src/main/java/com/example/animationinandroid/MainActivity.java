5a3db0c2b75c4bf9b21522398f0a4a3e07649b60
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-08-16 16:11:41 +0500

    Adding Images in Directory nd Different Animations implemented

--- app/src/main/java/com/example/animationinandroid/MainActivity.java
+++ app/src/main/java/com/example/animationinandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.animationinandroid;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private TextView txthelloworld,txt2,txt3,txt4;
+    private boolean isText,isImage;
+    private ImageView cat,dog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +37,40 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        txt4.animate().alpha(0f).setDuration(0);
+
         txt3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txt3.animate().alpha(0f).setDuration(3000);
-                txt4.animate().alpha(1f).setDuration(3000);
+                //txt3.animate().alpha(0f).setDuration(3000);
+                //txt4.animate().alpha(1f).setDuration(3000);
+
+                if(isText){
+                    txt3.animate().alpha(0f).setDuration(3000);
+                    txt4.animate().alpha(1f).setDuration(3000);
+                    isText=false;
+                }
+                else if(!isText){
+                    txt3.animate().alpha(1f).setDuration(3000);
+                    txt4.animate().alpha(0f).setDuration(3000);
+                    isText=true;
+                }
+
+            }
+        });
+        cat=findViewById(R.id.cat);
+        dog=findViewById(R.id.dog);
+        cat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isImage){
+                    cat.animate().alpha(0f).setDuration(3000);
+                    dog.animate().alpha(1f).setDuration(3000);
+                    isImage=false;
+                }else if(!isImage){
+                    cat.animate().alpha(1f).setDuration(3000);
+                    dog.animate().alpha(0f).setDuration(3000);
+                    isImage=true;
+                }
             }
         });
 
